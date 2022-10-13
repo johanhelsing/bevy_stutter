9dadff8eb6973291744107befcf80d3aeb0c429e
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-10-13 09:30:11 +0200

    Update examples

--- examples/detect.rs
+++ examples/detect.rs
@@ -3,25 +3,21 @@ use bevy_stutter::{RenderStutter, Stutter, StutterPlugin, UpdateStutter};
 
 fn main() {
     App::new()
-        // .insert_resource(WindowDescriptor {
-        //     mode: bevy::window::WindowMode::Fullscreen,
-        //     ..default()
-        // })
         .add_plugins(DefaultPlugins)
         .add_plugin(StutterPlugin)
         .add_startup_system(setup)
+        .add_system_to_stage(CoreStage::PreUpdate, detect_refresh_rate)
         .add_system(move_player_time_since_startup)
         .add_system(move_player_delta_time)
         .add_system(move_player_fixed_delta_time)
         .add_system(move_player_catch_up)
-        .add_system(move_player_catch_up_simple)
-        .add_system(move_player_golden)
         .run();
 }
 
 const MOVE_SPEED: f32 = 0.5;
-const TARGET_REFRESH_RATE: f64 = 60.0;
-const TARGET_REFRESH_INTERVAL: f64 = 1.0 / TARGET_REFRESH_RATE;
+
+struct RefreshRate {
+}
 
 #[derive(Component)]
 struct TimeSinceStartup;
@@ -56,7 +52,7 @@ fn move_player_fixed_delta_time(
     mut players: Query<&mut Transform, With<FixedDeltaTime>>,
     mut acc: Local<f64>,
 ) {
-    *acc += TARGET_REFRESH_INTERVAL;
+    *acc += 1.0 / 60.0;
     for mut tx in players.iter_mut() {
         tx.translation.x = f32::sin(*acc as f32 * MOVE_SPEED) * 500.0;
     }
@@ -74,7 +70,7 @@ fn move_player_catch_up(
 ) {
     let dt = time.delta_seconds_f64();
 
-    if (TARGET_REFRESH_INTERVAL - dt).abs() < 0.001 {
+    if (1.0 / 60.0 - dt).abs() < 0.001 {
         *good_frames += 1;
         if *good_frames >= 2 {
             if *good_frames == 2 {
@@ -82,7 +78,7 @@ fn move_player_catch_up(
             }
             // healthy frame rate
             *acc = 0.0;
-            *t += TARGET_REFRESH_INTERVAL
+            *t += 1.0 / 60.0
         } else {
             // still recovering
             *acc += dt;
@@ -97,58 +93,10 @@ fn move_player_catch_up(
         *acc += dt;
     };
 
-    while *acc > TARGET_REFRESH_INTERVAL {
-        *t += TARGET_REFRESH_INTERVAL;
-        *acc -= TARGET_REFRESH_INTERVAL;
+    while *acc > 1.0 / 60.0 {
+        *t += 1.0 / 60.0;
+        *acc -= 1.0 / 60.0;
     }
-
-    for mut tx in players.iter_mut() {
-        tx.translation.x = f32::sin(*t as f32 * MOVE_SPEED) * 500.0;
-    }
-}
-
-#[derive(Component)]
-struct CatchUpSimple;
-
-fn move_player_catch_up_simple(
-    mut players: Query<&mut Transform, With<CatchUpSimple>>,
-    mut t: Local<f64>,
-    time: Res<Time>,
-) {
-    let dt = time.delta_seconds_f64();
-
-    if dt > TARGET_REFRESH_INTERVAL * 1.3 {
-        info!("dropped frame, catching up {:.1}", dt * 1000.0);
-        *t += TARGET_REFRESH_INTERVAL;
-    }
-
-    *t += TARGET_REFRESH_INTERVAL;
-
-    for mut tx in players.iter_mut() {
-        tx.translation.x = f32::sin(*t as f32 * MOVE_SPEED) * 500.0;
-    }
-}
-
-#[derive(Component)]
-struct Golden4AndASixth;
-
-fn move_player_golden(
-    mut players: Query<&mut Transform, With<Golden4AndASixth>>,
-    mut t: Local<f64>,
-    time: Res<Time>,
-) {
-    let dt = time.delta_seconds_f64();
-
-    let delta_updates = dt * 240.0;
-
-    info!("delta_updates {delta_updates}");
-
-    let delta_updates = delta_updates.round() as i32;
-
-    // don't run slower than 30 Hz or faster than 240 Hz
-    let delta_updates = delta_updates.clamp(1, 8);
-
-    *t += (delta_updates as f64) / 240.0;
 
     for mut tx in players.iter_mut() {
         tx.translation.x = f32::sin(*t as f32 * MOVE_SPEED) * 500.0;
@@ -160,7 +108,7 @@ fn setup(mut commands: Commands) {
 
     commands.spawn().insert(RenderStutter(Stutter {
         probability: 0.00004,
-        millis: 32,
+        millis: 16,
     }));
 
     commands.spawn().insert(UpdateStutter(Stutter {
@@ -178,18 +126,6 @@ fn setup(mut commands: Commands) {
             ..default()
         })
         .insert(TimeSinceStartup);
-
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::WHITE,
-                custom_size: Some(Vec2::new(32.0, 32.0)),
-                ..default()
-            },
-            transform: Transform::from_translation(Vec3::Y * 200.0),
-            ..default()
-        })
-        .insert(Golden4AndASixth);
 
     commands
         .spawn_bundle(SpriteBundle {
@@ -226,16 +162,4 @@ fn setup(mut commands: Commands) {
             ..default()
         })
         .insert(CatchUp);
-
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::WHITE,
-                custom_size: Some(Vec2::new(32.0, 32.0)),
-                ..default()
-            },
-            transform: Transform::from_translation(Vec3::Y * -300.0),
-            ..default()
-        })
-        .insert(CatchUpSimple);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{thread::sleep, time::Duration};
+
+use bevy::{
+    ecs::query::QueryItem,
+    prelude::*,
+    render::{
+        extract_component::{ExtractComponent, ExtractComponentPlugin},
+        RenderApp, RenderStage,
+    },
+};
+
+pub struct StutterPlugin;
+
+#[derive(Clone)]
+pub struct Stutter {
+    pub probability: f32,
+    pub millis: u64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Component, Clone)]
+pub struct RenderStutter(pub Stutter);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Component, Clone)]
+pub struct UpdateStutter(pub Stutter);
+
+impl ExtractComponent for RenderStutter {
+    type Query = &'static Self;
+    type Filter = ();
+
+    fn extract_component(item: QueryItem<Self::Query>) -> Self {
+        item.clone()
+    }
+}
+
+impl Plugin for StutterPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(update_stutter);
+        app.add_plugin(ExtractComponentPlugin::<RenderStutter>::default());
+        let render_app = app.get_sub_app_mut(RenderApp);
+        if let Ok(render_app) = render_app {
+            render_app.add_system_to_stage(RenderStage::Render, render_stutter);
+        }
+    }
+}
+
+fn render_stutter(stutters: Query<&RenderStutter>) {
+    for stutter in stutters.iter() {
+        if fastrand::f32() < stutter.0.probability {
+            info!("sleeping for {} in render", stutter.0.millis);
+            sleep(Duration::from_millis(stutter.0.millis));
+        }
+    }
+}
+
+fn update_stutter(stutters: Query<&UpdateStutter>) {
+    for stutter in stutters.iter() {
+        if fastrand::f32() < stutter.0.probability {
+            info!("sleeping for {} in update", stutter.0.millis);
+            sleep(Duration::from_millis(stutter.0.millis));
+        }
     }
 }
